1ee03ddc6b18ecb631bcdef1fe784b431672a4b9
MahyParaquett <mahyaraparaquett@gmail.com>
2023-08-22 22:43:21 -0300

    correções da FK

--- TrabalhoIndividual/Script. trabalho individual.sql
+++ TrabalhoIndividual/Script. trabalho individual.sql
@@ -1,41 +1,42 @@
 --Modelo fisico
 create database trabalhoind
 
-create table denuncia (
-den_cd_id serial,
-den_tx_vitima varchar(50),
-den_tx_agressor varchar(50),
-den_tx_local varchar(50),
-den_dt_dataCaso date,
-primary key(den_cd_id)
-);
 create table delegacia (
 del_cd_id serial,
 del_tx_delegado varchar(50),
 del_tx_policial varchar(50),
 del_int_sala integer,
 del_dt_data date,
-fk_den_cd_id integer,
-primary key (del_cd_id),
-foreign key (fk_den_cd_id) references denuncia(den_cd_id)
+primary key (del_cd_id));
+
+create table denuncia (
+den_cd_id serial,
+den_tx_vitima varchar(50),
+den_tx_agressor varchar(50),
+den_tx_local varchar(50),
+den_dt_dataCaso date,
+fk_del_cd_id integer,
+primary key(den_cd_id),
+foreign key (fk_del_cd_id) references delegacia(del_cd_id)
 );
 
 --Inserindo registros
-insert into denuncia (den_tx_vitima, den_tx_agressor, den_tx_local, den_dt_dataCaso)  
+insert into delegacia(del_tx_delegado, del_tx_policial, del_int_sala, del_dt_data)
  values
- ('Maria', 'Robson','Rua', '20230807'),
- ('Joana', 'João','Casa', '20230701'),
- ('Diana', 'Ricardo','Casa', '20230502'),
- ('Juliana', 'Luis','Rua', '20230107'),
- ('Fernanda', 'Zé','Casa', '20230615');
+ ('Paulo', 'César','1', '20230820'),
+ ('Flavia', 'César','2', '20230801'),
+ ('Flavia', 'João','1', '20230509'),
+ ('Paulo', 'Alfredo','3', '20230101'),
+ ('Flavia', 'César','1', '20230615');
 
-insert into delegacia(del_tx_delegado, del_tx_policial, del_int_sala, del_dt_data, fk_den_cd_id)
+
+insert into denuncia (den_tx_vitima, den_tx_agressor, den_tx_local, den_dt_dataCaso,fk_del_cd_id)  
  values
- ('Paulo', 'César','1', '20230820', 1),
- ('Flavia', 'César','2', '20230801', 2),
- ('Flavia', 'João','1', '20230509', 3),
- ('Paulo', 'Alfredo','3', '20230101', 4),
- ('Flavia', 'César','1', '20230615', 5);
+ ('Maria', 'Robson','Rua', '20230807',1),
+ ('Joana', 'João','Casa', '20230701',2),
+ ('Diana', 'Ricardo','Casa', '20230502',3),
+ ('Juliana', 'Luis','Rua', '20230107',4),
+ ('Fernanda', 'Zé','Casa', '20230615',5);
 
 --Consultas
 --selecionar os registros onde a delegada Flavia homologou, 
